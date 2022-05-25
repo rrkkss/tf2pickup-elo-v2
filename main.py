f65738a7ef2486ba95d77b7640fa69b040a2aabb
rrkkss <rrkkss>
2022-05-26 01:20:20 +0200

    first

---
+++ main.py
@@ -0,0 +1,66 @@
+class player:
+    def __init__(self, id, nick, elo, gamesCount, wins, loses, redGames, redLoses, redWins, bluGames, bluLoses, bluWins,
+                scoutGames, scoutPlayTime, scoutDPM, scoutKPM, scoutKD, scoutKDA, scoutACC, scoutKills, scoutAssists, scoutDeaths,
+                soldierGames, soldierPlayTime, soldierDPM, soldierKPM, soldierKD, soldierKDA, soldierAirshots, soldierKills, soldierAssists, soldierDeaths,
+                demoGames, demoPlayTime, demoDPM, demoKPM, demoKD, demoKDA, demoAirshots, demoKills, demoAssists, demoDeaths,
+                medicGames, medicPlayTime, medicDPM, medicKPM, medicKD, medicKDA, medicHeals, medicUbers, medicHPM):
+        self.id = id
+        self.nick = nick
+        self.elo = elo
+        self.gamesCount = gamesCount
+        self.wins = wins
+        self.loses = loses
+        self.redGames = redGames
+        self.redLoses = redLoses
+        self.redWins = redWins
+        self.bluGames = bluGames
+        self.bluLoses = bluLoses
+        self.bluWins = bluWins
+        
+        self.scoutGames = scoutGames
+        self.scoutPlayTime = scoutPlayTime
+        self.scoutDPM = scoutDPM
+        self.scoutKPM = scoutKPM
+        self.scoutKD = scoutKD
+        self.scoutKDA = scoutKDA
+        self.scoutACC = scoutACC
+        self.scoutKills = scoutKills
+        self.scoutAssists = scoutAssists
+        self.scoutDeaths = scoutDeaths
+
+        self.soldierGames = soldierGames
+        self.soldierPlayTime = soldierPlayTime
+        self.soldierDPM = soldierDPM
+        self.soldierKPM = soldierKPM
+        self.soldierKD = soldierKD
+        self.soldierKDA = soldierKDA
+        self.soldierAirshots = soldierAirshots
+        self.soldierKills = soldierKills
+        self.soldierAssists = soldierAssists
+        self.soldierDeaths = soldierDeaths
+        
+        self.demoGames = demoGames
+        self.demoPlayTime = demoPlayTime
+        self.demoDPM = demoDPM
+        self.demoKPM = demoKPM
+        self.demoKD = demoKD
+        self.demoKDA = demoKDA
+        self.demoAirshots = demoAirshots
+        self.demoKills = demoKills
+        self.demoAssists = demoAssists
+        self.demoDeaths = demoDeaths
+
+        self.medicGames = medicGames
+        self.medicPlayTime = medicPlayTime
+        self.medicDPM = medicDPM
+        self.medicKPM = medicKPM
+        self.medicKD = medicKD
+        self.medicKDA = medicKDA
+        self.medicHeals = medicHeals
+        self.medicUbers = medicUbers
+        self.medicHPM = medicHPM
+
+player_list = []
+
+def createPlayer(id, nick):
+    player_list.append(player(id, nick, 1600, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
