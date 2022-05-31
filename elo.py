4c9bbd53ad4640ffaabcf8493e3de5a03bee3d92
rrkkss <rrkkss>
2022-05-31 23:59:39 +0200

    added predictions and refactorized elo methods

--- elo.py
+++ elo.py
@@ -14,14 +14,15 @@ def count_elo(playerElo, team, redElo, redScore, bluElo, bluScore):
         BF = 1
 
     if team == 'Red':
-        winChance = 1/(1+10**((bluElo - playerElo)/400))
-        return count_new_elo(playerElo, winChance, RF)
+        return count_new_elo(playerElo, count_win_chance(bluElo, playerElo), RF)
 
     elif team == 'Blu':
-        winChance = 1/(1+10**((redElo - playerElo)/400))
-        return count_new_elo(playerElo, winChance, BF)
+        return count_new_elo(playerElo, count_win_chance(redElo, playerElo), BF)
 
     return exception.EloCouldntBeCalculated
 
 def count_new_elo(playerElo, winChance, factor):
     return playerElo + 32*(factor - winChance)
+
+def count_win_chance(enemyElo, yourElo):
+    return (1/(1+10**((enemyElo - yourElo)/400)))
