9818f179e302a8c31e4b7baa8db4d2e7ee988b01
rrkkss <you@example.com>
2022-11-21 19:55:39 +0100

    cleaned up codebase

--- parse.py
+++ parse.py
@@ -13,7 +13,7 @@ canAddBonusElo = True
 countEloIndividually = False
 canSkipShitters = True
 
-def get_logs():
+def init_parse():
     search = input("\nEnter log title keyword, def 'tf2pickup.cz' => ") or 'tf2pickup.cz'
     wait = input("Enter wait time inbetween logs, def 0.4 => ") or 0.4
     wait = is_wait_number_valid(wait)
@@ -22,38 +22,36 @@ def get_logs():
     can_skip_shitters(input("Skip people with less then 5 games in the final log? [y / n]; def y => ") or 'y')
     set_elo_factor(input("Set elo factor [number]; def 32 => ") or 32)
 
+    get_logs(search, wait)
+
+def change_search_term(wait):
+    search = input("\nEnter different keyword' => ")
+    get_logs(search, wait)
+
+def get_logs(search, wait):
     url = 'http://logs.tf/api/v1/log?title=' + search + '&limit=5000'
     print(f"\nparsing from: {url}")
 
     try:
-        j = requests.get(url).json()
+        json = requests.get(url).json()
     except Exception as e:
         print(f"{e} \n json couldn't be parsed")
         quit()
 
-    results = list(j.values())[1]
+    logList = create_log_list(json)
 
-    if search == 'tf2pickup.cz':
-        results = results - 24 # comment below, it was 24 of them
-
-    if results > 0:
-        print(f"Found {results} results\n")
-        parse_logs(create_log_list(j, search), wait, results)
+    if len(logList) > 0:
+        print(f"Found {logList.__len__()} results\n")
+        parse_logs(logList, wait, len(logList))
     else:
         print(f"Found {results} results, try another search term")
-        get_logs()
+        change_search_term(wait)
 
-def create_log_list(json, title):
+def create_log_list(json):
     logList = []
 
-    for i in json:
-        if i == 'logs':
-            for k in json[i]:
-                n = list(k.values())
-                if title == 'tf2pickup.cz':
-                    if int(n[0]) < 2865412: # first actual czech pug, before that 24 dmixes were under the same title
-                        break
-                logList.append(n[0])
+    for log in json['logs']:
+        logList.append(log['id'])
 
     return logList
 
@@ -62,46 +60,46 @@ def parse_logs(logList, wait, results):
     # for i in logList: # for testing purposes
     for i in reversed(logList):
         count += 1
-        # if count > 10:
-        #     break
+        if count > 100:
+            break
         time.sleep(wait)
         url = 'https://logs.tf/json/' + str(i)
 
         try:
-            j = requests.get(url).json()
-            if j['success'] == True:
+            json = requests.get(url).json()
+            if json['success'] == True:
                 print(f"OK - {url} [{count}/{results}]")
-            if is_shit_log(j['teams']):
+            if is_shit_log(json['teams']):
                 print(f"~~~ Shit log detected, skipping")
                 continue
         except:
             print(f"FAILED - {url} [{count}/{results}]: nothing to parse, skipping. Wait time probably too SHORT")
             continue
 
-        get_data_from_log(j)
+        get_data_from_log(json)
 
-def get_data_from_log(j):
-    nicks = dict(j['names']).items()
+def get_data_from_log(json):
+    nicks = json['names']
     teamRed = []
     teamRedElo = []
     teamBlu = []
     teamBluElo = []
 
-    for k, v in nicks:
+    for k, v in nicks.items():
         if is_player_added(k) == False:
             playerList.append(player.createPlayer(k, v))
     
-    gameLength = int(j['length'])
-    scoreRed = get_scores_from_data(j, 'Red')
-    scoreBlu = get_scores_from_data(j, 'Blue')
+    gameLength = int(json['length']) # in seconds
+    scoreRed = get_scores_from_json(json, 'Red')
+    scoreBlu = get_scores_from_json(json, 'Blue')
 
-    for i in j['players'].items():
-        playerID = i[0]
-        playerInfo = dict(i[1].items())
+    for p in json['players'].items():
+        playerID = p[0]
+        playerInfo = dict(p[1].items())
         playerClass = playerInfo['class_stats'][0]['type']
         playerClassTime = playerInfo['class_stats'][0]['total_time']
         playerTeam = playerInfo['team']
-        playerDPM = playerInfo['dapm']         # damage per minute
+        playerDPM = playerInfo['dapm']          # damage per minute
         playerKPD = playerInfo['kpd']           # kill per death
         playerKAPD = playerInfo['kapd']         # kill + assist per death
         playerDMG = playerInfo['dmg']
@@ -159,21 +157,18 @@ def get_data_from_log(j):
         loop_over_team(i, 'Blu', teamRedElo, scoreRed, teamBluElo, scoreBlu)
         
 def is_shit_log(teams):
-    for i in teams.items():
-        if i[1]['kills'] < 32:
+    for team in teams.values():
+        if team['kills'] < 32:
             return True
     return False
 
-def get_scores_from_data(j, team):
-    for x in j["teams"].items():
+def get_scores_from_json(json, team):
+    for x in json['teams'].items():
         if x[0] == team:
-            return x[1]["score"]
+            return x[1]['score']
 
 def is_player_added(id):
-    for i in playerList:
-        if i.id == id:
-            return True
-    return False
+    return False if next((player for player in playerList if player.id == id), None) == None else True
 
 def get_player_elo(id):
     for i in playerList:
@@ -185,9 +180,9 @@ def loop_over_team(id, playerTeam, teamRedElo, scoreRed, teamBluElo, scoreBlu):
     if countEloIndividually:
         eloInFocus = get_player_elo(id)
     elif playerTeam == 'Red':
-        eloInFocus =get_average_elo(teamRedElo)
+        eloInFocus = get_average_elo(teamRedElo)
     elif playerTeam == 'Blu':
-        eloInFocus =get_average_elo(teamBluElo)
+        eloInFocus = get_average_elo(teamBluElo)
 
     set_player_elo(
         id, elo.count_elo(
@@ -203,26 +198,19 @@ def get_player_nick(id):
             return i.nick
 
 def set_player_elo(id, elo):
-    for i in playerList:
-        if i.id == id:
-            try:
-                if i.bonusElo > -40:
-                    i.eloOld = i.eloNew
-                    i.eloNew = elo + i.bonusElo
-            except:
-                print(f"elo couldnt be set for '{get_player_nick(id)}', probably due to being subbed out")
+    if next((player for player in playerList if player.id == id), None) != None :
+        try:
+            if i.bonusElo > -40:
+                i.eloOld = i.eloNew
+                i.eloNew = elo + i.bonusElo
+        except:
+            print(f"elo couldnt be set for '{get_player_nick(id)}', probably due to being subbed out")
 
 def set_player_bonus_elo(id, elo):
-    for i in playerList:
-        if i.id == id:
-            i.bonusElo = elo
+    if next((player for player in playerList if player.id == id), None) != None : player.bonusElo = elo
 
-def get_average_elo(list):
-    count = 0
-    for i in list:
-        count += float(i)
-
-    return float(count/len(list))
+def get_average_elo(playerList):
+    return float(sum(playerList) / len(playerList))
 
 def can_add_bonus_elo(input): # def true
     global canAddBonusElo
@@ -260,17 +248,16 @@ def set_elo_factor(num):
 def is_wait_number_valid(num):
     isNumberValid = False
 
-    while isNumberValid == False:  
-        try:
-            num = float(num)
-            if num >= 0:
-                isNumberValid = True
-            else:
-                num = list(num) # definitely ugly design, but it works hey
-        except:
+    if not is_number_valid(num):
+        while isNumberValid == False:
             num = input(f"Not a valid number, enter a new one => ")
+            if is_number_valid(num):
+                isNumberValid = True
+    
+    return float(num)
 
-    return num
+def is_number_valid(num):
+    return True if num.isdigit() else False
 
 def add_player_stats(
     id, playerClass, playerClassTime, playerTeam, scoreBlu, scoreRed,
@@ -278,14 +265,13 @@ def add_player_stats(
     playerHR, playerAirshots, playerKills, playerAssists, playerDeaths,
     playerHeal, playerUbers, playerUD, playerCPC, playerKPM
 ):
-    for i in playerList:
-        if i.id == id:
-            i = stats.set_stats(
-                i, playerClass, playerClassTime, playerTeam, scoreBlu, scoreRed,
-                playerDPM, playerKPD, playerKAPD, playerDMG, playerDT, playerDAPD,
-                playerHR, playerAirshots, playerKills, playerAssists, playerDeaths,
-                playerHeal, playerUbers, playerUD, playerCPC, playerKPM
-            )
+    if next((player for player in playerList if player.id == id), None) != None :
+        player = stats.set_stats(
+            i, playerClass, playerClassTime, playerTeam, scoreBlu, scoreRed,
+            playerDPM, playerKPD, playerKAPD, playerDMG, playerDT, playerDAPD,
+            playerHR, playerAirshots, playerKills, playerAssists, playerDeaths,
+            playerHeal, playerUbers, playerUD, playerCPC, playerKPM
+        )
 
 def compare_string(str1, str2):
     if str1 == str2:
